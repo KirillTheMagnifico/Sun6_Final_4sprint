f0849b47520624104df0cc4fe63f4c9916ccd870
KirillTheMagnifico <96779884+KirillTheMagnifico@users.noreply.github.com>
2023-03-14 01:20:41 +0300

    Initial commit

---
+++ venv/tests/locators/order_page_locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+class OrderPageLocators:
+
+    inputs = [By.XPATH, "//input[contains(@class, 'Input_Responsible__1jDKN')]"]
+    metro_station_input = [By.CLASS_NAME, 'select-search__input']
+    metro_station = [By.CLASS_NAME, 'select-search__row']
+    next_button = [By.XPATH, "//button[contains(text(), 'Далее')]"]
+    second_page = [By.XPATH, "//input[contains(@class, 'Input_Responsible__1jDKN')]"]
+    date = [By.XPATH, "//div[contains(@class, 'react-datepicker__day--selected')]"]
+    dropdown_control = [By.CLASS_NAME, 'Dropdown-control']
+    dropdown_option = [By.CLASS_NAME, 'Dropdown-option']
+    black_checkbox = [By.ID, 'black']
+    grey_checkbox = [By.ID, 'grey']
+    order_button = [By.XPATH, "//button[contains(text(), 'Заказать') and contains(@class, 'Button_Middle__1CSJM')]"]
+    yes_button = [By.XPATH, "//button[contains(text(), 'Да') and contains(@class, 'Button_Middle__1CSJM')]"]
+    text = [By.CLASS_NAME, 'Order_ModalHeader__3FDaJ']
